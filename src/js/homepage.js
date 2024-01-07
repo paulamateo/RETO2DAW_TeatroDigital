ac65626dbbd643aaa0234f95d4cf54eebffa6319
Paula Mateo <a26619@svalero.com>
2024-01-07 16:27:22 +0100

    [FEATURES/API_GETGENRE] Nueva organización de carpetas para las imágenes

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     scrollToShowsSection();
 
     let grid = document.getElementById('shows-grid__musical-genre');
+
     fetch('http://localhost:3000/shows/genre/musical')
         .then(response => response.json())
         .then(shows => {
@@ -25,51 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-/* <div class="grid-item grid-item--image-1">
-<h2>Divina Comedia</h2>
-<button class="button button--index" type="submit" id="button-show-5">VER ENTRADAS</button>
-</div>
-<div class="grid-item grid-item--image-2">
-<h2>Bodas de Sangre</h2>
-<button class="button button--index" type="submit" id="button-show-8">VER ENTRADAS</button>
-</div>
-<div class="grid-item grid-item--image-3">
-<h2>Edipo Rey</h2>
-<button class="button button--index" type="submit" id="button-show-6">VER ENTRADAS</button>
-</div> */
-
-
-
-// fetch('http://localhost:3000/shows')
-//     .then(response => response.json())
-//     .then(shows => {
-//       shows.forEach(show => {
-//         const showItem = document.createElement('div');
-//         showItem.classList.add('container-item');
-
-//         showItem.innerHTML = `
-//           <div class="genre">${show.genre}</div>
-//           <img src="${show.poster}" alt="${show.title}"/>
-//           <div class="title">
-//             <h3>${show.title}</h3>
-//           </div>
-//         `;
-
-//         showItem.addEventListener('click', function() {
-//           window.location.href = `show.html?title=${show.title}&id=${show.id}`;
-//         })
-
-//         container.appendChild(showItem);
-//       });
-//     })
-//     .catch(error => console.error('Error fetching data:', error));
-
-
-
-
-
-
-
 function changeMenuStyleOnScroll() {
     var header = document.getElementById('header');
     var headerPopup = document.getElementById('header__popup');
@@ -79,12 +35,12 @@ function changeMenuStyleOnScroll() {
 
     if (scrollPosition > 50) {
         header.classList.add('scrolled');
-        logoImage.src = '../img/logo_blanco.png';
+        logoImage.src = '../img/elements/logo_blanco.png';
         searchContainer.classList.add('scrolled');
         headerPopup.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
-        logoImage.src = '../img/logo_negro.png';
+        logoImage.src = '../img/elements/logo_negro.png';
         searchContainer.classList.remove('scrolled');
         headerPopup.classList.remove('scrolled');
     }
