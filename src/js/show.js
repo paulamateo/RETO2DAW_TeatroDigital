cb7c77dda6e60a2542980491badb308d22c41de8
Paula Mateo <licencias@svalero.com>
2024-01-09 21:30:19 +0100

    [API_SEATS] Ubicación de las butacas en los tickets (columnas)

--- src/js/show.js
+++ src/js/show.js
@@ -41,13 +41,13 @@ function createTicket(seatNumber) {
     ticketItem.classList.add('individual-ticket');
     ticketItem.dataset.seatNumber = seatNumber;
     ticketItem.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="trash-icon" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="trash-icon" id="trash-icon" viewBox="0 0 16 16">
             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
         </svg>
         <p><strong>Entrada para "EDIPO REY"</strong></p>
         <p>BUTACA ${seatNumber}</p>
         <div class="individual-ticket__data">
-            <p>Fila | Columna </p>
+            <p>Fila ${calculateRow(seatNumber)} | Columna ${calculateColumn(seatNumber)}</p>
             <p><strong>15,90€</strong></p>
         </div>
     `;
@@ -63,4 +63,18 @@ function removeTicket(seatNumber) {
             containerTickets.removeChild(ticket);
         }
     }
+}
+
+function calculateRow(seatNumber) {
+    if (seatNumber <= 60) {
+        return Math.ceil(seatNumber / 15);
+    } else {
+        const remainingSeats = seatNumber - 60;
+        return Math.ceil(remainingSeats / 14) + 4;
+    }
+}
+
+function calculateColumn(seatNumber) {
+    const columns = 12;
+    return (seatNumber - 1) % columns + 1;
 }
