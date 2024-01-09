444a83ec87cefd0bc5b0d99d641fb2dfe0ba349f
Paula Mateo <a26619@svalero.com>
2024-01-09 16:51:29 +0100

    [API_SEATS] Creación de tickets individuales de cada butaca

--- src/js/show.js
+++ src/js/show.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    initializeSeats();
-});
-
-
-function initializeSeats() {
     const areaSeatsContainer = document.getElementById('area-seats');
     const seatsPerRow = 15;
     const totalSeats = 135;
@@ -26,13 +21,46 @@ function initializeSeats() {
             seatItem.classList.add('seat');
             seatItem.textContent = seatCounter++;
             areaSeatsContainer.lastChild.appendChild(seatItem);
+
+            seatItem.addEventListener('click', function () {
+                seatItem.classList.toggle('selected');
+
+                if (seatItem.classList.contains('selected')) {
+                    createTicket(seatItem.textContent);
+                } else {
+                    removeTicket(seatItem.textContent);
+                }
+            });
         }
     }
+});
 
-    const seats = document.querySelectorAll('.seat');
-    seats.forEach((seat) => {
-        seat.addEventListener('click', function () {
-            seat.classList.toggle('selected');
-        });
-    });
+function createTicket(seatNumber) {
+    const containerTickets = document.getElementById('selected-tickets');
+    let ticketItem = document.createElement('div');
+    ticketItem.classList.add('individual-ticket');
+    ticketItem.dataset.seatNumber = seatNumber;
+    ticketItem.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="trash-icon" viewBox="0 0 16 16">
+            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+        </svg>
+        <p><strong>Entrada para "EDIPO REY"</strong></p>
+        <p>BUTACA ${seatNumber}</p>
+        <div class="individual-ticket__data">
+            <p>Fila | Columna </p>
+            <p><strong>15,90€</strong></p>
+        </div>
+    `;
+    containerTickets.appendChild(ticketItem);
+}
+
+function removeTicket(seatNumber) {
+    const containerTickets = document.getElementById('selected-tickets');
+    const tickets = containerTickets.getElementsByClassName('individual-ticket');
+
+    for (let ticket of tickets) {
+        if (ticket.dataset.seatNumber === seatNumber) {
+            containerTickets.removeChild(ticket);
+        }
+    }
 }
