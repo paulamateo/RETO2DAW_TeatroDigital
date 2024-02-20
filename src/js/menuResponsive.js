e658ec5ea6921d163a3b34fd384b32d8669a66e7
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-20 21:41:29 +0100

    [FEATURES/2EVALUACION] Popup del panel de admin

--- src/js/menuResponsive.js
+++ src/js/menuResponsive.js
@@ -7,12 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
         headerPopup.classList.toggle('active');
     })
 
+    var buttonPanel = document.getElementById('button-actions__panel');
+    var overlayPopUp = document.getElementById('overlay__actions-popup');
+    var closePanelAccess = document.getElementById('button__close-popup-panel-access');
+    buttonPanel.addEventListener('click', function() {
+        overlayPopUp.classList.toggle('active');
+    })
+
+    closePanelAccess.addEventListener('click', function() {
+        overlayPopUp.classList.remove('active');
+    })
+
+
+
+
+    
+
     //Cambio de logos blanco y negro
     document.addEventListener('scroll', function() {
         var header = document.getElementById('header');
         var headerPopup = document.getElementById('header__popup');
         var logoImage = document.getElementById('logoImage');
         var searchContainer = document.getElementById('search');
+        var buttonLogin = document.getElementById('button-actions__login');
+        var buttonPanel = document.getElementById('button-actions__panel');
         var scrollPosition = window.scrollY;
     
         if (scrollPosition > 50) {
@@ -20,11 +38,15 @@ document.addEventListener('DOMContentLoaded', () => {
             logoImage.src = '../img/elements/logo_blanco.png';
             searchContainer.classList.add('scrolled');
             headerPopup.classList.add('scrolled');
+            buttonLogin.classList.add('scrolled');
+            buttonPanel.classList.add('scrolled');
         }else {
             header.classList.remove('scrolled');
             logoImage.src = '../img/elements/logo_negro.png';
             searchContainer.classList.remove('scrolled');
             headerPopup.classList.remove('scrolled');
+            buttonLogin.classList.remove('scrolled');
+            buttonPanel.classList.remove('scrolled');
         }
     });
 
