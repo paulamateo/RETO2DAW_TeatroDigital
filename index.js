aff6a672c2a05ff044f8346f2fef13ca103a71ae
paula_mb <a26619@svalero.com>
2024-03-19 03:19:05 +0100

    Actualización del proyecto para hacer la parte de Diseño de la 2EV

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express()
 const fs = require('fs')
 const cors = require('cors')
 const port = 3000
+const path = require('path')
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'src')));
 
 let shows = [];
 
